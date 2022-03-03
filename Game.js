7bcf64e49dc278659b040be7559d1277c2c118d3
IMAT13 <Aliqaviimat13@gmail.com>
2022-03-03 11:02:15 +0330

    Adding some basic variables to js and a function in td for test

---
+++ Game.js
@@ -0,0 +1,10 @@
+var Turn="playerA";
+var GameArray=[[0,0,0],[0,0,0],[0,0,0]];//0 for not selected , A for player 1 and B for player 2
+function SetImg(e){
+    var TagetTD=e.srcElement;
+    var TDimg =document.createElement("img") ;
+    TDimg.src="circle.png";
+    TagetTD.appendChild(TDimg);
+}
+
+
