aaafc001d634c8a4bc9112fb09f39ca1abcfd2e2
IMAT13 <Aliqaviimat13@gmail.com>
2022-03-03 11:48:53 +0330

    Game basics

--- Game.js
+++ Game.js
@@ -1,10 +1,24 @@
 var Turn="playerA";
-var GameArray=[0,0,0,0,0,0,0,0,0];//0 for not selected , A for player 1 and B for player 2
+var GameArray=new Array(0,0,0,0,0,0,0,0,0);//0 for not selected , 1 for player 1 and 2 for player 2
 function SetImg(e){
     var TagetTD=e.srcElement;
-    var TDimg =document.createElement("img") ;
-    TDimg.src="circle.png";
-    TagetTD.appendChild(TDimg);
-}
+    var TDNum=(eval(TagetTD.id)-1);
+    if(GameArray[TDNum]==0){
+      if(Turn=="playerA"){
+        var TDimg =document.createElement("img") ;
+        TDimg.src="circle.png";
+        TagetTD.appendChild(TDimg);
+        Turn="playerB";
+        GameArray[TDNum]=1;
+      }  
+      else if(Turn=="playerB"){
+        var TDimg =document.createElement("img") ;
+        TDimg.src="multiply.png";
+        TagetTD.appendChild(TDimg);
+        Turn="playerA";
+        GameArray[TDNum]=2;
+      }
+    }  
 
+}
 
