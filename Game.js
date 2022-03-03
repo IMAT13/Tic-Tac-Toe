355bd6c15f1a27c8d1e817e923b4ad3102fe76ef
IMAT13 <Aliqaviimat13@gmail.com>
2022-03-03 11:07:04 +0330

    No need for 2D array

--- Game.js
+++ Game.js
@@ -1,5 +1,5 @@
 var Turn="playerA";
-var GameArray=[[0,0,0],[0,0,0],[0,0,0]];//0 for not selected , A for player 1 and B for player 2
+var GameArray=[0,0,0,0,0,0,0,0,0];//0 for not selected , A for player 1 and B for player 2
 function SetImg(e){
     var TagetTD=e.srcElement;
     var TDimg =document.createElement("img") ;
